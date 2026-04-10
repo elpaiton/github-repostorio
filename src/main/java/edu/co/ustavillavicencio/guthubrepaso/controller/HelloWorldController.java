83199eeb9996ce278944ef6df72a7e772e835c06
Javier Charry <javierandrescharry@gmail.com>
2026-04-10 10:39:57 -0500

    add: se agrego un hello world controller

---
+++ src/main/java/edu/co/ustavillavicencio/guthubrepaso/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package edu.co.ustavillavicencio.guthubrepaso.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+
+
+public class HelloWorldController {
+    @GetMapping("/world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
