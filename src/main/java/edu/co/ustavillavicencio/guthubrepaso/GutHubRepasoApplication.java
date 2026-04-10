7c889d9393c79a4532e700715f02434e4046137a
Javier Charry <javierandrescharry@gmail.com>
2026-04-10 10:27:59 -0500

    firts commit

---
+++ src/main/java/edu/co/ustavillavicencio/guthubrepaso/GutHubRepasoApplication.java
@@ -0,0 +1,13 @@
+package edu.co.ustavillavicencio.guthubrepaso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutHubRepasoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GutHubRepasoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/co/ustavillavicencio/guthubrepaso/GutHubRepasoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.co.ustavillavicencio.guthubrepaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GutHubRepasoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
